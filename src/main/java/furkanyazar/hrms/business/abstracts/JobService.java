ca93cca6e847e7089193a337858471432ac0d839
Furkan Yazar <frknyz1@gmail.com>
2021-05-11 23:21:51 +0300

    jobs listed with api

---
+++ src/main/java/furkanyazar/hrms/business/abstracts/JobService.java
@@ -0,0 +1,11 @@
+package furkanyazar.hrms.business.abstracts;
+
+import java.util.List;
+
+import furkanyazar.hrms.entities.concretes.Job;
+
+public interface JobService {
+	
+	List<Job> getAll();
+
+}
---
+++ src/main/java/furkanyazar/hrms/business/concretes/JobManager.java
@@ -0,0 +1,28 @@
+package furkanyazar.hrms.business.concretes;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import furkanyazar.hrms.business.abstracts.JobService;
+import furkanyazar.hrms.dataAccess.abstracts.JobDao;
+import furkanyazar.hrms.entities.concretes.Job;
+
+@Service
+public class JobManager implements JobService {
+	
+	private JobDao jobDao;
+
+	@Autowired
+	public JobManager(JobDao jobDao) {
+		super();
+		this.jobDao = jobDao;
+	}
+
+	@Override
+	public List<Job> getAll() {
+		return jobDao.findAll();
+	}
+
+}
